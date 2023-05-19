ca417bd43a2bccd8bfe26ed8897308fed7cd05b4
luka-ciric-under <luka.ciric@underit.rs>
2023-05-19 20:20:14 +0200

    Fixed login, added auth-context, style

--- Frontend/web2-frontend/src/components/Login/Login.jsx
+++ Frontend/web2-frontend/src/components/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import authService from "../../services/authService"
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../contexts/auth-context";
+import "./styles.css"
 
 const Login = () => {
     const [loginForm, setLoginForm] = useState({});
+    const context = useContext(AuthContext);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         if(!loginForm.email || !loginForm.password) {
@@ -12,33 +15,39 @@ const Login = () => {
             return;
         }
 
-        authService.login(loginForm);
+        await context.onLogin(loginForm);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-            <label>Email:</label>
-            <input
-                type="email"
-                id="email"
-                value={loginForm.email}
-                onChange={e => setLoginForm({...loginForm, email: e.target.value})}
-                required
-            />
-            </div>
-            <div>
-            <label>Password:</label>
-            <input
-                type="password"
-                id="password"
-                value={loginForm.password}
-                onChange={e => setLoginForm({...loginForm, password: e.target.value})}
-                required
-            />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={loginForm.email}
+                    onChange={e => setLoginForm({...loginForm, email: e.target.value})}
+                    required
+                />
+                </div>
+                <div>
+                <label>Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={loginForm.password}
+                    onChange={e => setLoginForm({...loginForm, password: e.target.value})}
+                    required
+                />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                {"You don't have account? "}
+                <Link to={"/register"}>Register</Link>
+            </p>
+        </div>
     )
 }
 
---
+++ Frontend/web2-frontend/src/contexts/auth-context.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import api from "../api/api";
+import jwtDecode from "jwt-decode";
+import {useNavigate} from 'react-router-dom'
+
+const AuthContext = React.createContext({
+    token: null,
+    onLogout: () => {},
+    // eslint-disable-next-line no-unused-vars
+    onLogin: (loginData) => {},
+});
+
+export const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setToken(sessionStorage.getItem('token'));
+    }, []);
+
+    const loginHandler = async(loginData) => {
+        try {
+            const res = await api.post('/auth/login', loginData)
+            if(!res)
+                return;
+
+            setToken(res.data.token);
+            localStorage.setItem('token', res.data.token); 
+            navigate("/home")
+        } catch (e){
+            alert(e.response.data.Exception);
+        }
+    };
+
+    const logoutHandler = () => {
+        setToken(null);
+        localStorage.clear();
+        navigate("/")
+    };
+
+    const userType = () => {
+        try {
+            if(!token)
+                return null;
+            const tokenDecoded = jwtDecode(token);
+            console.log("🚀 ~ file: auth-context.js:48 ~ userType ~ tokenDecoded:", tokenDecoded)
+            return tokenDecoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+            ;
+        } catch(e) {
+            console.log(e);
+        }
+    };
+
+    return (
+        <AuthContext.Provider
+        value={{
+            token: token,
+            onLogout: logoutHandler,
+            onLogin: loginHandler,
+            type: userType
+        }}>
+            
+            {// eslint-disable-next-line react/prop-types
+                props.children
+            }       
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
