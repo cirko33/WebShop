c0d6a38769f73b2c325b413e3dcece5695bd58a9
luka-ciric-under <luka.ciric@underit.rs>
2023-05-26 16:38:43 +0200

    Adding CRA, Modules etc

---
+++ Frontend/web2-front/src/components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../contexts/auth-context";
+import classes from './Login.module.css'
+
+const Login = () => {
+  const [loginForm, setLoginForm] = useState({});
+  const context = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!loginForm.email || !loginForm.password) {
+      alert("All fields required.");
+      return;
+    }
+
+    await context.onLogin(loginForm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <div>
+          <label className={classes.label}>Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={loginForm.email}
+            onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
+            required
+            className={classes.input}
+          />
+        </div>
+        <div>
+          <label className={classes.label}>Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={loginForm.password}
+            onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
+            required
+            className={classes.input}
+          />
+        </div>
+        <button type="submit" className={classes.submitButton}>Login</button>
+      </form>
+      <p className={classes.paragraph}>
+        {"You don't have an account? "}
+        <Link to={"/register"} className={classes.link}>Register</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Frontend/web2-front/src/services/userService.js
@@ -0,0 +1,41 @@
+import api from "../api/api"
+
+
+const register = async (data) => {
+    try {
+        await api.post("auth/register", data, { headers: { "Content-Type":"multipart/form-data" }});
+    }
+    catch(e) {
+        alert(e.response.data.Exception);
+        return Promise.reject(e);
+    }
+}
+
+const getUser = async () => {
+    try {
+        const res = await api.get("profile");
+        return res.data;
+    }
+    catch(e) {
+        alert(e.response.data.Exception);
+        return Promise.reject(e);
+    }
+}
+
+const setUser = async (data) => {
+    try {
+        await api.put("profile", data, { headers: { "Content-Type":"multipart/form-data" }});
+    }
+    catch(e) {
+        alert(e.response.data.Exception);
+        return Promise.reject(e);
+    }
+}
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    register,
+    getUser,
+    setUser,
+}
